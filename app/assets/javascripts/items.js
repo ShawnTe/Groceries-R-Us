8fb510faba8fec520ba1c8d00d94e16367f5f8b0
shawn <shawn@naturalprofessional.com>
2019-03-16 11:47:34 -0700

    Add jquery

---
+++ app/assets/javascripts/items.js
@@ -0,0 +1,11 @@
+
+// put listener on list
+// catch onchange events
+// capture id, and value
+// send to items#update_to_get to change items value
+// send updated value and checked presence to form
+
+$(document).ready(function() {
+
+  console.log("We are SOOOOOO ready!")
+});
